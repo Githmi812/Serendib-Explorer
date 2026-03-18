e1db05f801cfa44ba58b8e7eb4c1cee7d74a488d
Githmi812 <gitmiyovimma@gmail.com>
2026-03-18 19:20:24 +0530

    Add database connection requirement to api_contact.php

--- api_contact.php
+++ api_contact.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
 require_once "functions.php";
+require_once "db.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
 
